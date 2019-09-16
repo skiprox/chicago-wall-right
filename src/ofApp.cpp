dc88991995f59b3a2a7838fc4690ab98403db940
skiprox <seanmichaelscanlan@gmail.com>
2019-09-16 15:22:07 -0500

    Updates to text placement

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -134,56 +134,56 @@ void ofApp::setupAnimations(){
 	 */
 	milleniumFalconText = TextTyping(
 		"HISTORY OF THE MONTJUÏC FAIRGROUND\n\nIn 1915, Pich i Pon, the acting Mayor of Barcelona\nand owner of an electric utility company, proposed\nthe creation of the Montjuïc fairground to host an\nInternational Exhibition for showcasing the\nadvances electric power industries. This became\nknown as the 1929 Barcelona International Exhibi-\ntion, where the architect Mies van der Rohe\ndesigned the famous German pavilion, an icon of\nmodernism. The timing of the International Exhibi-\ntion coincided with The Great Depression, provok-\ning the sudden abandonment of many pavilions by\nits exhibitors. The International Exhibition was a\nfinancial disaster for the city, so much that the\nSpanish dictator Primo de Rivera censored any\ndetail regarding its cost. To pay all the debt that it\nhad incurred, the State emitted bonds, but this\nstrategy failed to raise the necessary capital. The\ndebt was so high that a new special city tax had to\nbe implemented for paying 14 million pesetas\nevery year for the subsequent 34 years. Scandals\nover the misappropriation of funds continued to\nplague la Fira de Barcelona. In the 1980s, when the\nmunicipal servants began preparing the 1992\nOlympic Games, they discovered that the City\nCouncil was still paying the debt of the 1929\nInternational Exhibition.",
-		glm::vec2(310, 900),
+		glm::vec2(310, 800),
 		ofColor(255),
 		10,
 		250
 	);
 	companiesCenterText = TextTyping(
 		"THE NEW NEIGHBORHOOD AND CULTURE\n\nDespite taking place in municipal land, the\nmega-events organized by Fira de Barcelona at\nthe Montjuïc fairground use vast public resources\nto support the propaganda of global capitalism\nand its corporate landscape. Their constant \nmarketing, their conferences and macro concerts\nare part of the contemporary spectacle that treats\ncitizens as a mass of consumers, indifferent to\nany future challenge to its well-being, such as the\nclimate emergency, the disruption of artificial\nintelligence, the rise of neo-fascism, poverty, the\nwealth gap, labor exploitation or patriarchy. For \ndecades, Fira de Barcelona has taken advantage\nof public assets to enrich the private corporations\n, their shareholders, and the perpetuation of the\nculture of mass consumption that they represent.",
-		glm::vec2(fixedWidth/2.0, 800),
+		glm::vec2(fixedWidth/2.0 - 40, 800),
 		ofColor(255),
 		10,
 		250
 	);
 	companiesRightText = TextTyping(
 		"AN OPAQUE NETWORK OF PUBLIC-PRIVATE\nENTITIES RULES KEY URBAN COMMONS\n\nThe neoliberal restructuring of the European Union\n project had dramatic consequences on the\n socio-spatial configurations of its cities. In Spain,\n led by a series of privatization friendly govern\nments, a significant effort was made to make \nurban regions attractive to foreign direct invest\nment. Barcelona, as most other European cities,\ncentered its objectives on becoming competitive,\nprompting it to offer to global private investors an\narray of highly favorable fiscal, infrastructural, and\npublic incentives to attract their capital. For the\nmanagement of this new investment, a powerful\nand opaque network of public-private corpora\ntions was weaved. Apart from the port, the airport,\nFira de Barcelona and the Chamber of Com\nmerce, the Consortium of the Free-trade Zone, the\nTourism Consortium of Barcelona and Mercabarna\nare remarkable knots in this network. The role of\nthese public-private partnerships has been to\nplunder municipal land, taxes, infrastructure,\npeople, and goods from the public and give full\ncontrol of them to the global capitalist elites.",
-		glm::vec2(1510, 900),
+		glm::vec2(245, 700),
 		ofColor(255),
 		10,
 		250
 	);
 	centerLeftText = TextTyping(
 		"THE NEW NEIGHBORHOOD AND DEMOCRACY\n\nTo extend the concession of the Montjuïc\nfairground to Fira de Barcelona and to allocate 380\nmillion euros of public money to its renovation\nproject “Fira Univers” is a despotic, opaque,\nclientelistic, short-sighted and anti-democratic \nimposition. It is an example of the extent to which\nglobal capitalism infringes upon public infrastruc\nture and public resources to thrive. The decision of\nthe City Council was announced unilaterally and \nwithout making the conditions of the agreement\npublic; it is unknown how many years would the\nconcession be extended. It leaves the future of the\nlarge piece of centrally located municipal land in\nthe hands of very influential lobbies, with private \ninterests and an opaque management structure.\nThe struggle of #LaFiraOLaVida is not only to\nprotest against the blatant abuse of power but to\nco-design and co-develop an alternative and\nviable proposal for achieving the right to the city\nfor all. There is still time! Barcelona's civil society\nhas until 2025 to organize an open, transparent,\nrigorous, and genuinely participatory debate. The\nUrban Front will be there to support its organiza-\ntion.",
-		glm::vec2(225, 670),
+		glm::vec2(245, 670),
 		ofColor(255),
 		10,
 		250
 	);
 	buildingLeftText = TextTyping(
 		"REAL-ESTATE SPECULATION, GENTRIFICATION\nAND HOUSING CRISIS IN BARCELONA\n\nFor decades, Spanish housing policies have\nentirely centered on the promotion of private-own-\nership, fueled by mortgages, financialization,\nrent-deregulation, fiscal incentives for speculators,\nand all the characteristic traits of the global real\nestate markets. These neoliberal policies were at\nthe heart of the 2008 burst of the housing bubble,\nwhich provoked 500,000 evictions while the\nSpanish government rescued the banks with more\nthan 60,000 million euros. In Barcelona, the hous-\ning condition has worsened, and it is at a critical\npoint. This is due in part to the city's profile as a\nsignificant tourist attraction as well as for congress\ngoers and organizers. In 2015, thanks to the\nelectoral victory of progressive mayor Ada Colau,\nthe city approved the construction of 18,000 new\naffordable housing units for the next ten years and\npassed legislation to force private developers to\ndestine 30% of newly built units as affordable. Still,\nthese policies have not been sufficient to curve the\nincreasing gentrification and displacements.\nToday, more than 50% pay over 40% of their\nincome to housing, while 30,000 families are\nwaiting for social housing and 80% of youngsters\nare now incapable of leaving their parent's homes.",
-		glm::vec2(690, 650),
+		glm::vec2(300, 650),
 		ofColor(255),
 		10,
 		250
 	);
 	buildingRightText = TextTyping(
 		"THE BARCELONA AIRPORT\n\nWith more than 50 million passengers in 2018, the\n Barcelona airport is the sixth most transited in the\n European Union. It is situated at 10 minutes by car\n from the Montjuïc fairground. The airport is\n managed by a publicly-traded state enterprise\n known as AENA, a for-profit property of the\n Spanish state. The President of Fira de Barcelona,\n Pau Relat, has been at work, lobbying for a\n multi-billion euro project to expand Barcelona's\n airport. The projec argues for the urgent construc-\ntion of a 4th landin strip to be built on the sea.",
-		glm::vec2(1180, 700),
+		glm::vec2(1260, 700),
 		ofColor(255),
 		10,
 		250
 	);
 	planeText = TextTyping(
 		"CLIMATE CRISIS AND ENVIRONMENTAL\nPOLLUTION IN BARCELONA\n\nBarcelona is extremely vulnerable to the environ-\nmental crisis. At the global warming level, it is a\ncoastal city that is susceptive to water shortages\nand cyclical heatwaves. The predicted sea-level\nrise is expected to swallow beach fronts and flood\nentire neighborhoods, like Barceloneta. The\nincrease in temperatures will favor the introduction\nof tropical illnesses and the intensification of heavy\nstorms as well as long term drought. On the local\nscale, Barcelona is one of the most polluted cities\nin Europe. Its low air quality is responsible for\n3,500 premature deaths per year in its metropolitan\narea. Barcelona is also the European city most\npolluted by cruise ship fuel and the second throw-\ning more plastic waste the Mediterranean Sea. If\nthis was not enough, it also has some of the\ndensest motor traffic in Europe, at a ratio of 7,000\nvehicles per square kilometer —London has 1,200\nand Paris 1,500—. Over the years, Fira de Barcelo-\nna has hosted expositions for major polluting\nindustries. Its 2 million annual visitors generate\nmassive toxic emissions, water pollution, plastic\nwaste, and energy consumption.",
-		glm::vec2(250, 530),
+		glm::vec2(250, 630),
 		ofColor(255),
 		10,
 		250
 	);
 	shipText = TextTyping(
 		"THE PORT OF BARCELONA\n\nThe port of Barcelona is the most important\ncommodity trade and cruise ship facility of the\nMediterranean. It is managed by one of the most\ninfluential organizations in the region, another\nopaque public-private entity called Port de Barce-\nlona, with links to La Fira de Barcelona. It is rarely\nmentioned in the media, but has not been exempt\nfrom criminal proves related to illegal selling of\npublic space, as well as for corruption, illicit drug\ntrade and its relationship to Russian, Chinese and\nQatar mafias. It occupies a piece of public land\nmore extensive than the whole of Cerdá's Eixam-\nple district, fenced, secured by its port police,\nguarded by cameras and security infrastructure. It\nis a hidden site behind the Montjuïc hill where the\nvast majority of Barcelona's citizens have never\nset afoot.",
-		glm::vec2(1490, 960),
+		glm::vec2(1400, 860),
 		ofColor(255),
 		10,
 		250
